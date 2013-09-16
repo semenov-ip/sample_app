9714a82aeb58f3f0910365ae818d037fe5e8a83f
Igor <semenov-ip@mail.ru>
2013-09-16 14:21:34 +0400

    Finish user signup

--- Gemfile
+++ Gemfile
@@ -46,6 +46,8 @@ end
 
 group :test do
   gem 'capybara', '1.1.2'
+
+  gem 'factory_girl_rails', "~> 4.0"
 end
 
 # Use ActiveModel has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 SampleApp::Application.routes.draw do
-  
-  get "users/new"
 
-  root 'static_pages#home'
+  resources :users
 
-  get 'signup'   => 'users#new'
-  get 'help'     => 'static_pages#help'
-  get 'about'    => 'static_pages#about'
-  get 'contact'  => 'static_pages#contact'
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new',            via: 'get'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
